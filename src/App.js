1aba22103ed44fffc8cb0439710184feaa63edb0
onur <onur@onur-MacBook-Pro.local>
2024-04-08 22:34:01 +0300

    basic react test example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const product = {
+  id: 1,
+  name: "iPhone 15 Pro",
+  image: "https://onursahin.net/wp-content/uploads/2022/03/images.png",
+  price: 100,
+};
+
+function ProductCard() {
+  const [count, setCount] = useState(0);
+
+  const handleAddToCart = () => {
+    setCount(count + 1);
+  };
+
+  const handleRemoveFromCart = () => {
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>{product.price}</p>
+      <button onClick={handleAddToCart}>Add Cart</button>
+      <p>
+        <strong>Count: {count}</strong>
+      </p>
+      {count > 0 && (
+        <button onClick={handleRemoveFromCart}>Remove Cart</button>
+      )}
     </div>
   );
 }
 
-export default App;
+export default ProductCard;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// onursahin.net
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import ProductCard from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("ProductCard Component Test", () => {
+  it("The product card must be rendered successfully", () => {
+    render(<ProductCard />);
+
+    expect(screen.getByText("iPhone 15 Pro")).toBeInTheDocument(); 
+    expect(screen.getByText("100")).toBeInTheDocument();
+
+    const productImage = screen.getByAltText("iPhone 15 Pro"); 
+    expect(productImage).toBeInTheDocument(); 
+    expect(productImage.src).toBe(
+      "https://onursahin.net/wp-content/uploads/2022/03/images.png"
+    );
+  });
+
+  it("When the Add Cart button is clicked, the product must be added to the cart.", () => {
+    render(<ProductCard />);
+    fireEvent.click(screen.getByText("Add Cart"));
+    expect(screen.getByText("Count: 1")).toBeInTheDocument();
+  });
+
+  it("When the Remove Cart button is clicked, the product must be removed from the cart.", () => {
+    render(<ProductCard />);
+    fireEvent.click(screen.getByText("Add Cart")); 
+    fireEvent.click(screen.getByText("Remove Cart"));
+    expect(screen.getByText("Count: 0")).toBeInTheDocument();
+  });
 });
